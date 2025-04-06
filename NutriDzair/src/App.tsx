a866d1e2a626b0a0298a42cdf0a04d4c1d1ef84d
KhalilGH <khalil.ghanem@ensia.edu.dz>
2025-04-06 22:23:12 +0100

    working on inputs page

--- NutriDzair/src/App.tsx
+++ NutriDzair/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import MealPlanner from './pages/MealPlanner/MealPlanner';
+import './App.css';
 
 function App() {
 
   return (
-    <h1>Hello World</h1>
+    <MealPlanner/>
   )
 }
 
