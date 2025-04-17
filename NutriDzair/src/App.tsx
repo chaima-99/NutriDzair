9255030cb36f3a6c80592e2678a191de1d4400ae
chaima-amali <chaima.amali@ensia.edu.dz>
2025-04-17 22:15:16 +0100

    Refactor imports in App.tsx for better organization

--- NutriDzair/src/App.tsx
+++ NutriDzair/src/App.tsx
@@ -1,8 +1,8 @@
 // App.tsx
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Homepage';
-
-
+import MealPlanner from './pages/MealPlanner/MealPlanner';
+import Loading from './pages/loading';
 // Create router with all your routes
 const router = createBrowserRouter([
   {
