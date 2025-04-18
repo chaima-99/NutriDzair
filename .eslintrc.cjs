6bcd995475e282291f67ca9447e72296c5852ad9
KhalilGH <khalil.ghanem@ensia.edu.dz>
2025-04-18 17:59:35 +0100

    proejct file

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: ["react-app", "prettier"],
-    plugins: ["prettier", "react-hooks"],
-    rules: {
-      "prettier/prettier": "error",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-    },
-  };
---
+++ test.py
@@ -0,0 +1,2 @@
+a = [[1,2,3], [4,5,6]]
+print (6 in l for l in a)
